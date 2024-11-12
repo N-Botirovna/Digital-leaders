ced2e99abad76d72c31dbb326962cf1f88683c19
Mashkhura <mashhura2020@gmail.com>
2024-11-12 16:11:08 +0500

    grants,partners,events created in component folder

--- src/pages/Events/index.jsx
+++ src/pages/Events/index.jsx
@@ -1,3 +1,4 @@
+import EventsPage from "../../component/Events";
 import Navbar from "../../component/navbar";
 import Container from "../../component/ui/Container";
 const Events = () => {
@@ -5,7 +6,7 @@ const Events = () => {
     <div>
       <Container>
         <Navbar/>
-        <h1>Events</h1>
+       <EventsPage/>
       </Container>
     </div>
   )
